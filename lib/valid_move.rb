63efb3f69be42a6b029c9030d87962fb4ee19938
Corban <sonnemanc@gmail.com>
2021-06-28 20:27:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, position)
     spot = position.to_i - 1
   if !position_taken?(board, spot) && spot.between?(0, 8)
     true
-  else position_taken?(board, index)
+  else position_taken?(board, spot)
     false
   end
 end
