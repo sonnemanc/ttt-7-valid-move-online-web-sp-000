fa5130bea335ded9cde27308860d5fa78767e556
Corban <sonnemanc@gmail.com>
2021-06-28 19:54:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index.between?(0,8) == true
    return true
- else index > 8 || position_taken? == true
+ else index > 8 || position_taken?
    return false
  end
 end
